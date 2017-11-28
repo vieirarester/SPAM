85e0d298ccc3637969b3aa64db51b08b34bc73c9
Ester Vieira <vieirarester@gmail.com>
2017-11-28 14:18:29 -0300

    Retirando importação desnecessária

--- src/main/java/br/ifpe/garanhuns/spam/controladores/AlunoControlador.java
+++ src/main/java/br/ifpe/garanhuns/spam/controladores/AlunoControlador.java
@@ -9,8 +9,6 @@ import br.ifpe.garanhuns.spam.dao.AlunoDao;
 import br.ifpe.garanhuns.spam.dao.implementacoes.AlunoImplDao;
 import br.ifpe.garanhuns.spam.modelo.negocio.Aluno;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
