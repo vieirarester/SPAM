fa730a933c9be04646389dbb6c90698c00aba57b
João Witor Tenório Cavalcante Santos <joaowitort@gmail.com>
2017-12-04 15:22:25 -0300

    Criando dao da classe Usuario

---
+++ src/main/java/br/ifpe/garanhuns/spam/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ifpe.garanhuns.spam.dao;
+
+import br.ifpe.garanhuns.spam.modelo.negocio.Usuario;
+
+/**
+ *
+ * @author Joao 
+ */
+public interface UsuarioDao extends Dao<Usuario> {
+        public Usuario recuperarLogin(String login);
+
+}
