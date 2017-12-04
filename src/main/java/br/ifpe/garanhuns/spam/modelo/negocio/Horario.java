b8e8328a8728a76498df056e7f4bc076fc7adb8a
Ester Vieira <vieirarester@gmail.com>
2017-12-04 10:33:05 -0300

    Apagando importação

--- src/main/java/br/ifpe/garanhuns/spam/modelo/negocio/Horario.java
+++ src/main/java/br/ifpe/garanhuns/spam/modelo/negocio/Horario.java
@@ -1,7 +1,5 @@
 package br.ifpe.garanhuns.spam.modelo.negocio;
 
-import java.util.Date;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
