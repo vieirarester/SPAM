742b6d6767c43a9c42ae190d94dae2c644fdecf5
Ester Vieira <vieirarester@gmail.com>
2017-12-28 16:07:40 -0300

    Criando classe de implementação do Dao para Disciplina

---
+++ src/main/java/br/edu/ifpe/garanhuns/spam/dao/implementacoes/DisciplinaImplDao.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.garanhuns.spam.dao.implementacoes;
+
+import br.edu.ifpe.garanhuns.spam.dao.Dao;
+import br.edu.ifpe.garanhuns.spam.dao.DaoManagerHiber;
+import br.edu.ifpe.garanhuns.spam.modelo.negocio.Disciplina;
+import java.util.List;
+
+/**
+ *
+ * @author Ester
+ */
+public class DisciplinaImplDao implements Dao<Disciplina>{
+
+    @Override
+    public void inserir(Disciplina a) {
+        DaoManagerHiber.getInstance().persist(a);
+    }
+
+    @Override
+    public void deletar(Disciplina a) {
+        DaoManagerHiber.getInstance().delete(a);
+    }
+
+    @Override
+    public void atualizar(Disciplina a) {
+        DaoManagerHiber.getInstance().update(a);
+    }
+
+    @Override
+    public Disciplina recuperar(long id) {
+        try{
+            return (Disciplina) DaoManagerHiber.getInstance().recover("from Disciplina where id="+id);
+        }catch (IndexOutOfBoundsException excecao) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Disciplina> recuperarTodos() {
+        return DaoManagerHiber.getInstance().recover("from Disciplina");
+    }
+    
+}
