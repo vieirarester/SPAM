5acca5030ebdaee0348ae5018df008a7b776b1fa
Ester Vieira <vieirarester@gmail.com>
2017-12-28 15:46:58 -0300

    Criando a classe Disciplina

--- src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Disciplina.java
+++ src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Disciplina.java
@@ -5,10 +5,44 @@
  */
 package br.edu.ifpe.garanhuns.spam.modelo.negocio;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author Ester
  */
+@Entity
 public class Disciplina {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column
+    private String nome;
+    
+    public Disciplina(){
+        
+    }
+    
+    public Disciplina(long id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
     
 }
