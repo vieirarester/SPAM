012783c14ded53cd1e074848e5b0592ddd075ab2
Ester Vieira <vieirarester@gmail.com>
2018-01-03 23:49:00 -0300

    Criando método para setar monitor em disciplina

--- src/main/java/br/edu/ifpe/garanhuns/spam/controladores/MonitorControlador.java
+++ src/main/java/br/edu/ifpe/garanhuns/spam/controladores/MonitorControlador.java
@@ -31,16 +31,16 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @SessionScoped
 public class MonitorControlador {
-    
+
     MonitorDao monitorDao = null;
     AlunoDao alunoDao = null;
     Dao disciplinaDao = null;
-    
+
     private Monitor monitor;
     private Horario horario;
     private Usuario usuario;
     private Disciplina disciplina;
-    
+
     @PostConstruct
     public void inicializar() {
         monitorDao = new MonitorImplDao();
@@ -51,19 +51,19 @@ public class MonitorControlador {
         usuario = new Usuario();
         disciplina = new Disciplina();
     }
-    
+
     public Monitor getMonitor() {
         return monitor;
     }
-    
+
     public void setMonitor(Monitor monitor) {
         this.monitor = monitor;
     }
-    
+
     public Horario getHorario() {
         return horario;
     }
-    
+
     public void setHorario(Horario horario) {
         this.horario = horario;
     }
@@ -75,7 +75,7 @@ public class MonitorControlador {
     public void setDisciplina(Disciplina disciplina) {
         this.disciplina = disciplina;
     }
-    
+
     public boolean validarUsuario(String login) {
         boolean jaExiste;
         if (monitorDao.recuperarLogin(login) != null || alunoDao.recuperarLogin(login) != null) {
@@ -85,88 +85,101 @@ public class MonitorControlador {
         }
         return jaExiste;
     }
-    
+
+    public void inserirMonitorNaDisciplina() {
+
+        Monitor m = this.getMonitor();
+        Disciplina d = this.getDisciplina();
+
+        if (d.getMonitores() == null) {
+            d.setMonitores(new ArrayList<Monitor>());
+        }
+
+        d.getMonitores().add(m);
+        this.setDisciplina(d);
+    }
+
     public String inserirMonitor(Monitor monitor) {
-        
-            if (!validarUsuario(monitor.getUsuario().getLogin())) {
-                monitor.setHorarios(this.monitor.getHorarios());
-                monitor.setDisciplina(disciplina);
-                
-                String senhaCripto = Criptografia.criptografar(monitor.getUsuario().getSenha());
-                monitor.getUsuario().setSenha(senhaCripto);
-                
-                this.monitorDao.inserir(monitor);
-                
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O monitor foi cadastrado com sucesso!"));
-            } else {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Este usuário já existe!"));
-            }
+
+        if (!validarUsuario(monitor.getUsuario().getLogin())) {
+            monitor.setHorarios(this.monitor.getHorarios());
+            monitor.setDisciplina(disciplina);
+
+            String senhaCripto = Criptografia.criptografar(monitor.getUsuario().getSenha());
+            monitor.getUsuario().setSenha(senhaCripto);
+
+            this.monitorDao.inserir(monitor);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O monitor foi cadastrado com sucesso!"));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Este usuário já existe!"));
+        }
         return "";
     }
-    
+
     public void removerMonitor(Monitor monitor) {
         this.monitorDao.deletar(monitor);
-        
+
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O monitor foi deletado com sucesso!"));
-        
+
     }
-    
+
     public String alterarMonitor(Monitor monitor) {
         this.monitorDao.atualizar(monitor);
-        
+
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("O monitor foi alterado com sucesso!"));
         return "";
     }
-    
+
     public Monitor recuperarMonitor(long id) {
         return this.monitorDao.recuperar(id);
     }
-    
+
     public List<Monitor> recuperarTodosMonitor() {
         return this.monitorDao.recuperarTodos();
     }
-    
+
     public List<Horario> recuperarTodosHorario() {
         return this.monitor.getHorarios();
     }
-    
-    public List<Disciplina> recuperarTodosDisciplina(){
+
+    public List<Disciplina> recuperarTodosDisciplina() {
         return this.disciplinaDao.recuperarTodos();
     }
-    
+
     public boolean validarHorario(Horario horario) {
         String[] horaEntrada = horario.getHoraInicio().split(":");
         String[] horaSaida = horario.getHoraFim().split(":");
         int[] entrada = new int[2];
         int[] saida = new int[2];
-        
+
         for (int i = 0; i < 2; i++) {
             entrada[i] = Integer.parseInt(horaEntrada[i]);
             saida[i] = Integer.parseInt(horaSaida[i]);
         }
-        
+
         if (entrada[0] < saida[0] && horario.getDia() != null) {
             return true;
         } else if (entrada[1] < saida[1] && horario.getDia() != null && entrada[0] == saida[0]) {
             return true;
         }
-        
+
         return false;
     }
-    
+
     public String inserirHorario() {
-        
+
         Monitor m = this.getMonitor();
-        
+
         Horario h = new Horario();
         h.setDia(this.horario.getDia());
         h.setHoraInicio(this.horario.getHoraInicio());
         h.setHoraFim(this.horario.getHoraFim());
-        
+
         if (m.getHorarios() == null) {
             m.setHorarios(new ArrayList<Horario>());
         }
-        
+
         boolean existe = false;
         for (Horario hor : m.getHorarios()) {
             if (hor.getDia().equals(this.horario.getDia()) && hor.getHoraInicio().equals(this.horario.getHoraInicio()) && hor.getHoraFim().equals(this.horario.getHoraFim())) {
@@ -174,23 +187,23 @@ public class MonitorControlador {
                 break;
             }
         }
-        
+
         if (!existe) {
             if (validarHorario(h)) {
                 m.getHorarios().add(h);
                 this.setMonitor(m);
             } else {
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Formato de horário inválido. Hora de entrada posterior a hora de saída!"));
-                
+
             }
-            
+
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Horário já cadastrado!"));
         }
-        
+
         return "";
     }
-    
+
     public String removeHorario(Horario horario) {
         Monitor m = this.getMonitor();
         m.getHorarios().remove(horario);
