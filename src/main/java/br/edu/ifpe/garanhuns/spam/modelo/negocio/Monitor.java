8c7ad3a383b16ceae1cf5a9b8220bab98cf9d625
Ester Vieira <vieirarester@gmail.com>
2018-01-03 22:45:23 -0300

    Adicionando anotações na classe Monitor

--- src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Monitor.java
+++ src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Monitor.java
@@ -15,6 +15,7 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -33,6 +34,7 @@ public class Monitor implements Serializable, SampleEntity{
     @Embedded
     private Usuario usuario;
     @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn
     private Disciplina disciplina;
     @OneToMany (cascade = CascadeType.ALL)
     private List<Horario> horarios;
