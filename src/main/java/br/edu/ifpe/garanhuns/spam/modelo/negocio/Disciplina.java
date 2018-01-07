22c2338bbed810fe35dc9b88349bd769ba2381ce
Ester Vieira <vieirarester@gmail.com>
2018-01-07 09:35:48 -0300

    Adicionando anotação da lista de monitores

--- src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Disciplina.java
+++ src/main/java/br/edu/ifpe/garanhuns/spam/modelo/negocio/Disciplina.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -26,7 +27,7 @@ public class Disciplina implements Serializable, SampleEntity{
     private long id;
     @Column
     private String nome;
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Monitor> monitores;
     
     public Disciplina(){
